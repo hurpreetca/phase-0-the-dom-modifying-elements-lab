1ec13c7c753c27148241ed17fddae1611ee5900f
Harpreet Singh <hurpreet@icloud.com>
2023-04-08 12:23:11 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let body= document.querySelector('main');
+body.remove();
+
+
+
+let newHeader= document.createElement('H1');
+newHeader.id= "victory";
+newHeader.textContent= "YOUR-NAME is the champion";
